cadc04f184ae9d3cc8cfb885d797460a24fe44ca
Michel Morais <valentimmichel@gmail.com>
2023-02-22 14:58:07 -0300

    Live version

---
+++ src/model/exceptions/BusinessException.java
@@ -0,0 +1,9 @@
+package model.exceptions;
+
+public class BusinessException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public BusinessException(String msg) {
+		super(msg);
+	}
+}
